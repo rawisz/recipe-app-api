e92383e0158d5f401f03d69c322673f7954824db
Rawisz <r.wiszowaty@gmail.com>
2024-03-23 22:06:26 +0100

    Install postrgres adapter.

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def substract(x, y):
+    """Substrac x from y and return result"""
+    return y - x
